85df889692e90f2903da5e0e7d1ca1a3d0e7b569
Manikantareddy Sakhamreddy <manikantasakham09@gmail.com>
2025-01-06 14:11:22 -0500

    Updated [Dockerefile], main.py and added requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,18 @@
 from python3.9
 
-COPY 
+## Install R and update the necessary packages
+
+RUN apt-get update && apt-get install -y r-base
+
+##Install python dependencies
+
+COPY requirements.txt
+RUN pip install -r requirements.txt
+
+#COPY application files
+COPY  ..
+
+#Run the FASTAPI application
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+
--- app/main.py
+++ app/main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, uploadFile, File
 from pydantic import BaseModel
 from pymongo import  MongoClient
-from typing import list
+import subprocess
+
 
 app = FastAPI()
 
-@app.get("/")
-async def related():
-	return {"message": "hello world"}
+#connect to MongoDB
+client  = MongoClient("mongodb://localhost:27017/")
+
+database = client["rnaseq_analysis"]
+
+@app.post("/upload")
+async def upload_files(files: List[UploadFile] = File(...)):
+	#save the uploaded files and store metadata in MongoDB
+	for file in files:
+	   contents = await file.read()
+	   with openff"data/{file.filename}", "wb") as f:
+		f.write(contents)
+	   db.files.insert_one({filename": file.filename})
+	return {"message": "File uploaded successfully"}
+
+@app.post("/analyze")
+async def run_rnaseq_analysis():
+
+
+
 
 
